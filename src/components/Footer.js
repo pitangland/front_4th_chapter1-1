bc002766b6ae957fb1c01106e9a4402ad1eb1348
pitang <wpeffect@gmail.com>
2024-12-17 21:30:05 +0900

    feat: 재사용 가능한 컴포넌트 구성

---
+++ src/components/Footer.js
@@ -0,0 +1,7 @@
+const Footer = () => {
+  return `<footer class="bg-gray-200 p-4 text-center">
+        <p>&copy; 2024 항해플러스. All rights reserved.</p>
+      </footer>`;
+};
+// 함수 선언이므로 그 함수를 호출하면 문자열이 반환된다.
+export default Footer;
