be1258336545f9ea10ad632bfd82fe7ca6bfa4ae
pitang <wpeffect@gmail.com>
2024-12-19 01:28:44 +0900

    refactor: HTML태그에 값 업데이트

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -1,5 +1,6 @@
 import { getUser, saveUser, removeUser } from "../store";
 import { navigate } from "../main";
+
 const ProfilePage = () => {
   const user = getUser();
 
@@ -39,7 +40,7 @@ const ProfilePage = () => {
             type="email"
             id="email"
             name="email"
-            value="hong@example.com"
+            value=${getUser().email || ""}
             class="w-full p-2 border rounded"
           />
         </div>
@@ -54,9 +55,7 @@ const ProfilePage = () => {
             name="bio"
             rows="4"
             class="w-full p-2 border rounded"
-          >
-안녕하세요, 항해플러스에서 열심히 공부하고 있는 홍길동입니다.</textarea
-          >
+          >${getUser().bio || ""}</textarea>
         </div>
         <button
           type="submit"
@@ -90,10 +89,6 @@ const ProfilePage = () => {
     const updateUser = { ...user, username, email, bio };
     saveUser(updateUser);
   });
-  //saveUser를 한 이후에 세팅해주기
-  document.getElementById("username").value = getUser().username;
-  document.getElementById("email").value = getUser().email;
-  document.getElementById("bio").value = getUser().bio;
 };
 
 export default ProfilePage;
