8d2cb76931d28c4dc5017f64784da0466b1e8569
pitang <wpeffect@gmail.com>
2024-12-20 05:01:37 +0900

    refactor: 이벤트 위임 방식으로 관리하며 동일한 class 값 추가

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,12 +7,12 @@ const Header = () => {
         </header>
         <nav id="topBar" class="bg-white shadow-md p-2 sticky top-14">
         <ul class="flex justify-around">
-            <li><a href="/" class="text-blue-600 font-bold">홈</a></li>
+            <li><a href="/" class="text-blue-600 font-bold nav-link">홈</a></li>
             ${
               isLoggedIn
-                ? `<li><a href="/profile" class="text-gray-600">프로필</a></li>
-            <li><a href="/login" class="text-gray-600" id="logout">로그아웃</a></li>`
-                : `        <li><a href="/login" class="text-gray-600">로그인</a></li>`
+                ? `<li><a href="/profile" class="text-gray-600 nav-link">프로필</a></li>
+            <li><a href="/login" class="text-gray-600 nav-link" id="logout">로그아웃</a></li>`
+                : `        <li><a href="/login" class="text-gray-600 nav-link" id="login">로그인</a></li>`
             }
         </ul>
         </nav>`;
